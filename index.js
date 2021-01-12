66b8841de0296c95f41eca5773c84e399118786b
AlexandreTadeuJunior <alexandre.tadeu1995@gmail.com>
2021-01-12 16:27:57 -0300

    add
    fazendo deploy no Heroku para subir app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./server/config/express');
 const config = require('./server/config/env');
 
 app.listen(config.portApi, () => {
+    // Mostrando que o APP está funcionando
   console.log(
     `API Server iniciada e lendo a porta ${config.portApi} (${config.env})`
   );
